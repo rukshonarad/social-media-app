e09b568c95a8b38e22a98e4d527483a46aab84a5
Rukshona Rad <rukshona.rad@gmail.com>
2023-11-20 09:07:48 -0500

    Create user signUp

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,3 +4,24 @@ import { bcrypt } from "../utils/bcrypt.js";
 import { v4 as uuid } from "uuid";
 import jwt from "jsonwebtoken";
 import { mailer } from "../utils/mailer.js";
+
+class UserService {
+    signUp = async (userInput) => {
+        const hashedPassword = await bcrypt.hash(userInput.password);
+        const activationToken = crypto.createToken();
+        const hashedActivationToken = crypto.hash(activationToken);
+        const user = await prisma.user.create({
+            data: {
+                ...userInput,
+                password: hashedPassword,
+                activationToken: hashedActivationToken
+            },
+            select: {
+                id: true
+            }
+        });
+
+        await mailer.sendActivationMail(userInput.email, activationToken);
+    };
+}
+export const userService = new UserService();
